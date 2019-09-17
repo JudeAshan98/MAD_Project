3b604fab37c27c64da2e2309b969575b287d2655
Judet <judeashanlakmal356@gmail.com>
2019-09-18 00:11:41 +0530

    First Noobs commit

---
+++ app/src/main/java/com/example/test/bonusLevel.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bonusLevel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bonus_level);
+    }
+}
---
+++ app/src/main/java/com/example/test/spalsh.java
@@ -0,0 +1,41 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class spalsh extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setMax(100);
+        progressBar.setProgress(0);
+
+        final Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    for (int i = 0; i <= 100; i++) {
+                        progressBar.setProgress(i);
+                        sleep(20);
+                    }
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(getApplicationContext(),settings.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
